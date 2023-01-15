3287dbb455164fd195aba96aec8ea3b46ac1b815
João Gabriel Godoy <joao.sgodoy@hotmail.com>
2023-01-14 21:55:58 -0300

    14.13

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +19,12 @@ import org.springframework.web.multipart.MultipartFile;
 import com.algaworks.algafood.api.assembler.FotoProdutoModelAssembler;
 import com.algaworks.algafood.api.model.FotoProdutoModel;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.service.CadastroProdutoService;
 import com.algaworks.algafood.domain.service.CatalogoFotoProdutoService;
+import com.algaworks.algafood.domain.service.FotoStorageService;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -31,11 +39,14 @@ public class RestauranteProdutoFotoController {
 	@Autowired
 	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
 	
+	@Autowired
+	private FotoStorageService fotoStorageService;
+	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoModel atualizarFoto(
 			@PathVariable Long restauranteId,
 			@PathVariable Long produtoId,
-			@Valid FotoProdutoInput fotoProdutoInput) {
+			@Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 		
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		FotoProduto foto = new FotoProduto();
@@ -48,9 +59,31 @@ public class RestauranteProdutoFotoController {
 		foto.setTamanho(arquivo.getSize());
 		foto.setNomeArquivo(arquivo.getOriginalFilename());
 		
-		FotoProduto fotoSalva = catalogoFotoProdutoSerivce.salvar(foto);
+		FotoProduto fotoSalva = catalogoFotoProdutoSerivce.salvar(foto, arquivo.getInputStream());
 		
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 	}
-
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public FotoProdutoModel buscar(@PathVariable Long restauranteId,
+			@PathVariable Long produtoId) {
+		FotoProduto fotoProduto = catalogoFotoProdutoSerivce.buscarOuFalhar(restauranteId, produtoId);
+		
+		return fotoProdutoModelAssembler.toModel(fotoProduto);
+	}
+	
+	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
+			@PathVariable Long produtoId) {
+		try {
+			FotoProduto fotoProduto = catalogoFotoProdutoSerivce.buscarOuFalhar(restauranteId, produtoId);
+			InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+			
+			return ResponseEntity.ok()
+					.contentType(MediaType.IMAGE_JPEG)
+					.body(new InputStreamResource(inputStream));
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.domain.service;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public interface FotoStorageService {
+	
+	InputStream recuperar(String nomeArquivo);
+	
+	void armazenar(NovaFoto novaFoto);
+	
+	void remover (String nomeArquivo);
+	
+	default void substituir(String nomeArquivoExistente, NovaFoto novaFoto) {
+		this.armazenar(novaFoto);
+		
+		if (nomeArquivoExistente != null) {
+			this.remover(nomeArquivoExistente);
+		}
+	}
+	
+	default String gerarNomeArquivo(String nomeOriginal) {
+		return UUID.randomUUID().toString() + "_" + nomeOriginal;
+	}
+	
+	@Getter
+	@Builder
+	class NovaFoto {
+		
+		private String nomeArquivo;
+		private InputStream inputStream;
+		
+	}
+
+}
