8e2ee763abf468a3ca3780eab78e1282d5fd19ad
João Gabriel Godoy <joao.sgodoy@hotmail.com>
2023-11-20 21:30:35 -0300

    18.13

--- src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
@@ -1,14 +1,21 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModel {
 	
+	@ApiModelProperty(value = "ID da cidades", example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "Florianópolis")
 	private String nome;
-	private String estado;
+	
+	private EstadoModel estado;
 
 }
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,15 +1,24 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,21 +26,103 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig implements WebMvcConfigurer {
 	
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-				.paths(PathSelectors.any())
-//				.paths(PathSelectors.ant("/restaurantes/*"))
-				.build()
+					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+					.paths(PathSelectors.any())
+	//				.paths(PathSelectors.ant("/restaurantes/*"))
+					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
+				.globalResponseMessage(RequestMethod.POST, globalPostResponseMessages())
+				.globalResponseMessage(RequestMethod.PUT, globalPutResponseMessages())
+				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));
 	}
 	
-	public ApiInfo apiInfo() {
+	private List<ResponseMessage> globalGetResponseMessages() {
+		return Arrays.asList(
+					new ResponseMessageBuilder()
+						.code(HttpStatus.BAD_REQUEST.value())
+						.message("Requisição inválida (erro do cliente)")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno servidor")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno servidor")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.NOT_ACCEPTABLE.value())
+						.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.build()
+				);
+	}
+	
+	private List<ResponseMessage> globalPostResponseMessages() {
+		return Arrays.asList(
+					new ResponseMessageBuilder()
+						.code(HttpStatus.BAD_REQUEST.value())
+						.message("Requisição inválida (erro do cliente)")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.NOT_ACCEPTABLE.value())
+						.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
+						.message("Requisição recusada porque o corpo está em um formato não suportado")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno servidor")
+						.build()
+				);
+	}
+	
+	private List<ResponseMessage> globalPutResponseMessages() {
+		return Arrays.asList(
+					new ResponseMessageBuilder()
+						.code(HttpStatus.BAD_REQUEST.value())
+						.message("Requisição inválida (erro do cliente)")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.NOT_ACCEPTABLE.value())
+						.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
+						.message("Requisição recusada porque o corpo está em um formato não suportado")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno servidor")
+						.build()
+				);
+	}
+	
+	private List<ResponseMessage> globalDeleteResponseMessages() {
+		return Arrays.asList(
+					new ResponseMessageBuilder()
+						.code(HttpStatus.BAD_REQUEST.value())
+						.message("Requisição inválida (erro do cliente)")
+						.build(),
+					new ResponseMessageBuilder()
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno servidor")
+						.build()
+				);
+	}
+	
+	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("AlgaFood - Godoy")
 				.description("API aberta para clientes e restaurantes")
