e72895fc9bb59c27652f0a0584d22374965b0612
João Gabriel Godoy <joao.sgodoy@hotmail.com>
2023-11-29 22:25:42 -0300

    19.3 - Hateoas

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.ResourceUriHelper;
 import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
 import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
 import com.algaworks.algafood.api.model.CidadeModel;
@@ -87,7 +88,11 @@ public class CidadeController implements CidadeControllerOpenApi {
 
 			cidade = cadastroCidadeService.salvar(cidade);
 
-			return cidadeModelAssembler.toModel(cidade);
+			CidadeModel cidadeModel =  cidadeModelAssembler.toModel(cidade);
+			
+			ResourceUriHelper.addUriInResponseHeader(cidadeModel.getId());
+			
+			return cidadeModel;
 		} catch (EstadoNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage(), e);
 		}
